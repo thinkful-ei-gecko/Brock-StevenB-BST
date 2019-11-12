e32a1fe000eaa8ffccba1abe7f494292d39ae70a
Steven Bull <stevenwbulljr@gmail.com>
2019-11-12 16:52:05 +0000

    finish problem 5

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 const BinarySearchTree = require('./bst');
 
-function main(){
+// Q5. Create an algorithm to find the height of a tree.
+function maxHeightFinder(root, num = 1) {
+  // here's just base case -- if you get an empty root(because you definitely will at some point, just return a depth of zero because there's nothing in the tree!
+  if (root === null) {
+    return 0;
+  }
+  // this is your terminating case. a leaf node doesn't have any children, and so the root at that point will have null value. at this point just return the depth/num
+  if (root.right === null && root.left === null) {
+    return num;
+  }
+  //Use Math.max to get the highest between the two root depths. the rest of this code is just handling the individual root cases.
+  if (root.right && root.left) {
+    return Math.max(
+      maxHeightFinder(root.right, num + 1),
+      maxHeightFinder(root.left, num + 1)
+    );
+  } 
+  else if (root.right !== null) {
+    return maxHeightFinder(root.right, num + 1);
+  } 
+  else {
+    return maxHeightFinder(root.left, num + 1);
+  }
+}
+
+function main() {
   let bst = new BinarySearchTree();
 
   bst.insert('E');
@@ -16,9 +41,22 @@ function main(){
   bst.insert('O');
   bst.insert('N');
 
-  console.log(bst.find('T'));
+  console.log(maxHeightFinder(bst));
 
+  //console.log(bst.find('T'));
 
-  console.log(bst);
+  //console.log(bst);
 }
 main();
+
+/* 4. what does this function do?
+function tree(t){
+    if(!t){
+        return 0;
+    }
+    return tree(t.left) + t.value + tree(t.right)
+}
+
+loops through the tree to add up all of the values of all of the nodes on both sides.
+
+*/
